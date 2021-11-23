13c43feed51fe5848ada571e0761efd0fe66f9d0
akukamil <chamilioner_kid@mail.ru>
2021-11-23 17:07:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1050,8 +1050,8 @@ function load_resources() {
 	//короткая ссылка на ресурсы
 	gres=game_res.resources;
 	
-	//let git_src="https://akukamil.github.io/melody/"
-	let git_src=""
+	let git_src="https://akukamil.github.io/melody/"
+	//let git_src=""
 	
 
 	
