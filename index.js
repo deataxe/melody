efa40b1d2a1cf12de060fcccc9a4c4f7285eef65
akukamil <chamilioner_kid@mail.ru>
2021-12-21 14:42:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -643,10 +643,7 @@ var auth = function() {
 				if (s.includes("vk.com")) {
 					game_platform="VK";
 					Promise.all([
-						this.loadScript('//ad.mail.ru/static/admanhtml/rbadman-html5.min.js'),
-						this.loadScript('//vk.com/js/api/adman_init.js'),
 						this.loadScript('https://unpkg.com/@vkontakte/vk-bridge/dist/browser.min.js')
-
 					]).then(function(){
 						help_obj.vk()
 					}).catch(function(e){
